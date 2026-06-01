c0d4b0e5f617a3577172519b639161ef1408e198
Zulal Akbulut <zulalakbulut2516@gmail.com>
2026-06-01 11:13:33 +0200

    fix(cypress): fix cucumber preprocessor setup and add e2e test structure

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,18 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   e2e: {
     specPattern: "e2e/**/*.feature",
     supportFile: false,
+
     async setupNodeEvents(on, config) {
-      await preprocessor.addCucumberPreprocessorPlugin(on, config);
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [preprocessor.createEsbuildPlugin(config)],
-        })
-      );
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on("file:preprocessor", createBundler());
+
       return config;
     },
   },
